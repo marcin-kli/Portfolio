50dcddbe92637aefacc66c516a3cf7e8a494d66b
marcin-kli <klimaszewski.m@gmail.com>
2021-01-17 20:22:21 +0000

    Add style.css, test contact form, move portfolio to index.html

--- run.py
+++ run.py
@@ -1,22 +1,29 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-
-@app.route("/projects/")
-def projects():
     data = []
     with open("data/portfolio.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("projects.html", page_title="Portf", projects=data)
+    return render_template(
+        "index.html", page_title="Portfolio", projects=data)
+
+
+@app.route("/projects", methods=["GET", "POST"])
+def projects():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+    return render_template("projects.html")
 
 
 if __name__ == "__main__":
